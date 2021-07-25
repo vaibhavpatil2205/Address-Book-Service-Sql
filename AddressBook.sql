1ae7721c4d2a091859e8a1c2c30e777a6f16eb13
unknown <vaibhavpatil5471@gmail.com>
2021-07-26 00:09:37 +0530

    Retrive person from table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,5 +1,33 @@
-#uc5  Delete person using FirstName
+#UC6  Retrieve Person belonging to a City or State from the Address Book
 
-DELETE FROM `address_book` 
+#1)Retrive contact using city 
+SELECT 
+    *
+FROM
+    address_book
 WHERE
-    `firstName` = 'Sunny';
+    city = 'pune';
+    
+#2)Retrive contact using city
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    city = 'Vimantal';
+    
+#3)Retrive contact using city
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    city = 'Mumbai';
+
+#4)Retrive contact using state
+SELECT 
+    *
+FROM
+    address_book
+WHERE
+    state = 'maharastra';   
