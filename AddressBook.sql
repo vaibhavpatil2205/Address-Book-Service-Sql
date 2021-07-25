b8412686c773d8e2d11c5ece06f291990f6f782d
unknown <vaibhavpatil5471@gmail.com>
2021-07-25 23:54:13 +0530

    Inserting new contacts into address book

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,13 +1,9 @@
-#uc2  create table for database
+#uc3  Insert new Contacts to Address Book
 
-CREATE TABLE address_book(
-    firstName VARCHAR(30) NOT NULL,
-    lastName VARCHAR(30) NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    city VARCHAR(20) NOT NULL,
-    state VARCHAR(20) NOT NULL,
-    zip VARCHAR(10) NOT NULL,
-    phoneNo VARCHAR(15) NOT NULL,
-    email VARCHAR(15) NOT NULL,
-    PRIMARY KEY (firstName)
-);
+insert  into address_book(firstName,lastname,address,city,state,zip,phoneNo,email) values
+ ('Vaibhav','Patil','Malegaon Road','Pune','Maharastra','424101','959595','vai@gmail.com'),
+ ('Sunny','Rajput','King Road','Chalisgaon','Maharastra','414565','956565','sun@gmail.com'),
+ ('Sagar','Singh','sb road','Vimantal','Maharastra','411111','937313','sag@gmail.com'),
+ ('Prajwal','Sheth','Laxmi road','mumbai','Maharastra','422233','995623','pra@gmail.com');
+ 
+select * from address_book;
