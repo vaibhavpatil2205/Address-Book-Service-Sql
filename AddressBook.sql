654965fdd21de68d53bbfafc3139a0a911991c2b
unknown <vaibhavpatil5471@gmail.com>
2021-07-26 00:31:14 +0530

    COUNT(city) AS NumberOfaddress_book FROM address_book

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,7 +1,3 @@
-#uc9  identify each Address Book with name and type.
+#UC10  COUNT(city) AS NumberOfaddress_book FROM address_book;
 
-alter table address_book add type varchar(30) after lastName;
-update  address_book set type='family' where firstName ='Vaibhav';
-update  address_book set type='friend' where firstName ='Sagar';
-update  address_book set type='profession' where firstName ='Prajwal';
-select * from address_book;
+select count(type) AS NumberOfaddress_book FROM address_book;    
