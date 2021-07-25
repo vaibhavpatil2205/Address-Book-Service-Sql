c8680e3d5018705c75fac8334cf50a6c5393f16f
unknown <vaibhavpatil5471@gmail.com>
2021-07-26 00:28:09 +0530

    identify each Address Book with name and type

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,7 +1,7 @@
-#uc8  Retrieve entries sorted alphabetically by Person’s name for a given city
+#uc9  identify each Address Book with name and type.
 
-SELECT 
-    firstName,city
-FROM
-    address_book
-ORDER BY firstName;
+alter table address_book add type varchar(30) after lastName;
+update  address_book set type='family' where firstName ='Vaibhav';
+update  address_book set type='friend' where firstName ='Sagar';
+update  address_book set type='profession' where firstName ='Prajwal';
+select * from address_book;
